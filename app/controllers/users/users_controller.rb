dbaaa2c333024da8278152e56064defc02c15f3f
SuhailLoya <suhail.loya@gmail.com>
2024-01-25 22:26:49 +0800

    updated

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -7,6 +7,7 @@ class Users::UsersController < ApplicationController
         puts "token"
         decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
         user_id = decoded_token[0]['user_id']
+        puts user_id
         current_user = User.find(user_id)
         render json: current_user
       else
